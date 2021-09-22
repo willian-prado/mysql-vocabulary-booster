903bdf183d9905f5f5030271699b73bb69ea6b2e
Willian Prado <willian_prado@hotmail.com>
2021-09-21 23:24:42 -0300

    Corrige o desafio 5*

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT 
     JOB_TITLE AS `Cargo`,
-    (MAX_SALARY - MIN_SALARY) AS `Variação salarial`,
+    (MAX_SALARY - MIN_SALARY) AS `Variação Salarial`,
     ROUND((MIN_SALARY / 12), 2) AS `Média minima mensal`,
     ROUND((MAX_SALARY / 12), 2) AS `Média máxima mensal`
 FROM
