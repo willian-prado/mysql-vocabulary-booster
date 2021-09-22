8b6b89291eb324179d34f845e2fc070368dbec13
Willian Prado <willian_prado@hotmail.com>
2021-09-21 22:56:48 -0300

    Desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email_usuario varchar(100))
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE total_jobs INT;
+    SELECT COUNT(e.FIRST_NAME) AS `total_empregos`
+	FROM hr.employees as e
+	INNER JOIN hr.job_history as jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+	WHERE e.EMAIL = email_usuario INTO total_jobs;
+    RETURN total_jobs;
+END $$
+
+DELIMITER ;
